587c60dd2ed46bc4dcc49fa84f38aafeb64d7145
Matias Rozanec <rozanecm@gmail.com>
2020-10-27 10:17:14 -0300

    reactivate thread pool in server - requests manager

--- server/common/server.py
+++ server/common/server.py
@@ -32,8 +32,8 @@ class Server:
         while self.keep_server_running.value:
             logging.info("in server loop")
             client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
-            # self.executor.submit(self.__handle_client_connection, client_sock)
+            # self.__handle_client_connection(client_sock)
+            self.executor.submit(self.__handle_client_connection, client_sock)
         logging.info("terminating main server")
 
     def __accept_new_connection(self):
